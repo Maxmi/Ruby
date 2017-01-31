b646e54b291985577be9e423b444a6199e7d511e
Gulmira Berdalieva <miramaxeast@yahoo.com>
2017-01-31 12:25:35 -0800

    moved  thankyou method

--- sample_app/app/controllers/static_pages_controller.rb
+++ sample_app/app/controllers/static_pages_controller.rb
@@ -10,14 +10,20 @@ class StaticPagesController < ApplicationController
   	@products = Product.limit(3)
 	end
 
+	# def thank_you
+	#   @name = params[:name]
+	#   @email = params[:email]
+	#   @message = params[:message]
+	#   ActionMailer::Base.mail(:from => @email,
+	#       :to => 'miramaxeast@yahoo.com',
+	#       :subject => "A new contact form message from #{@name}",
+	#       :body => @message).deliver_now
+	# end
 	def thank_you
 	  @name = params[:name]
 	  @email = params[:email]
 	  @message = params[:message]
-	  ActionMailer::Base.mail(:from => @email,
-	      :to => 'miramaxeast@yahoo.com',
-	      :subject => "A new contact form message from #{@name}",
-	      :body => @message).deliver_now
+	  UserMailer.contact_form(@email, @name, @message).deliver_now
 	end
 
 end
