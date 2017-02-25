2487e06fafec249992935e78e779873107a776c3
Gulmira Berdalieva <miramaxeast@yahoo.com>
2017-02-24 21:42:59 -0800

    comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,4 +10,10 @@ class CommentsController < ApplicationController
 	def destroy
 	end
 
+	private
+
+  def comment_params
+    params.require(:comment).permit(:user_id, :body, :rating)
+  end
+
 end
